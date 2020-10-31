97d8ae78cf288c111ba8696441539b01a63f094d
taiersistemas <ivanignacio.taier@gmail.com>
2020-10-31 12:06:33 -0300

    se comentarios de nuevo para ver todo el codigo en el pr

--- app/modules/demandante/screens/CategoryPreviewScreen/index.js
+++ app/modules/demandante/screens/CategoryPreviewScreen/index.js
@@ -1,152 +1,151 @@
-/*
-
-import React, { useContext, useRef, useState, useEffect } from "react";
-import {
-  View,
-  Dimensions,
-  Alert,
-  StyleSheet,
-  Image,
-  ScrollView,
-} from "react-native";
-import {
-  colors,
-  ui,
-  calculateSize,
-  typography,
-} from "../../../../shared/styles";
-import Text from "../../../../shared/components/atoms/Text";
-import { Screen } from "../../../../shared/components/organisms";
-import Animated, { Easing } from "react-native-reanimated";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { Settings, ChangeMode } from "../../../../shared/vectors";
-import useActions from "../../../../hooks/useActions";
 
 
-//los que agregue yo
-import { LinearGradient } from "expo-linear-gradient";
-//import MainHeader from './MainHeader';
-import ContactsCard from "../ContactsScreen/ContactsCard";
-import { images } from "./../../../../utils/images";
-
-const CategoryPreviewScreen = (props) => {
-  const contacts = {
-    recently: [
-      {
-        name: "Nahuel Cristofoli",
-        //skill:'Ingeniero Industrial',
-        photo: images.user_andres,
-        state: true,
-      },
-      {
-        name: "Mariano Busti",
-        // skill:'Couch Emprendedor',
-        photo: images.user_mariano,
-        state: false,
-      },
-      {
-        name: "Marcelo Ponti",
-        //skill:'Diseñador Grafico',
-        photo: images.user_marce,
-        state: true,
-      },
-    ],
-    all: [
-      {
-        name: "Alejandro DiLuca",
-        // skill:'Productor de cine',
-        photo: images.user_alejandro,
-        state: true,
-      },
-      {
-        name: "Nicolas Perazzo",
-        // skill:'Economista',
-        photo: images.user_nico,
-        state: false,
-      },
-      {
-        name: "Damian Grimberg",
-        // skill:'Desarrollador',
-        photo: images.user_dami,
-        state: false,
-      },
-    ],
-  };
-
-  const {
-    route: { params },
-  } = props;
-
-  const { image, title, subtitle } = params;
-
-  const insets = useSafeAreaInsets();
-
-  const styles = StyleSheet.create({
-    container: {
-      width: "95%",
-      justifyContent: "flex-start",
-      alignItems: "center",
-      backgroundColor: colors.backgroundGrey.primary,
-    },
-    catImag: {
-      width: "100%",
-      height: 250,
-      borderRadius: 10,
-    },
-    catTitle: {
-      textAlignVertical: "top",
-      position: "absolute",
-      color: "rgba(255,255,255,0.95)",
-    },
-    degradado: {
-      position: "absolute",
-      width: "100%",
-      height: "100%",
-    },
-    space: {
-      height: 50,
-      width: "100%",
-    },
-    spaceInitial: {
-      marginTop: "10%",
-      marginLeft: "5%",
-    },
-    mainContainer: {
-      marginTop: ui.padding,
-    },
-    title: {
-      paddingLeft: ui.padding + ui.borderRadius.borderRadius / 2,
-      textAlign: "left",
-      marginBottom: ui.padding,
-    },
-  });
-
-  return (
-    <View style={styles.container}>
-      <ScrollView scrollEventThrottle={1}>
-        <View style={styles.spaceInitial}>
-          <Image source={image} style={styles.catImag}></Image>
-          <LinearGradient style={styles.degradado} {...colors.greyGradient} />
-          <Text extraStyles={styles.catTitle} {...typography["title-28"]}>
-            {" "}
-            {`${title}`}{" "}
-          </Text>
-        </View>
-        <View style={styles.mainContainer}>
-          {contacts.recently.map((item, index) => (
-            <ContactsCard contact={item} key={`${item.name}->${index}`} />
-          ))}
-          {contacts.all.map((item, index) => (
-            <ContactsCard contact={item} key={`${item.name}->${index}`} />
-          ))}
-        </View>
-        <View style={styles.space} />
-      </ScrollView>
-    </View>
-  );
-};
-
-export default CategoryPreviewScreen;
+// import React, { useContext, useRef, useState, useEffect } from "react";
+// import {
+//   View,
+//   Dimensions,
+//   Alert,
+//   StyleSheet,
+//   Image,
+//   ScrollView,
+// } from "react-native";
+// import {
+//   colors,
+//   ui,
+//   calculateSize,
+//   typography,
+// } from "../../../../shared/styles";
+// import Text from "../../../../shared/components/atoms/Text";
+// import { Screen } from "../../../../shared/components/organisms";
+// import Animated, { Easing } from "react-native-reanimated";
+// import { useSafeAreaInsets } from "react-native-safe-area-context";
+// import { Settings, ChangeMode } from "../../../../shared/vectors";
+// import useActions from "../../../../hooks/useActions";
 
 
-*/
+// //los que agregue yo
+// import { LinearGradient } from "expo-linear-gradient";
+// //import MainHeader from './MainHeader';
+// import ContactsCard from "../ContactsScreen/ContactsCard";
+// import { images } from "./../../../../utils/images";
+
+// const CategoryPreviewScreen = (props) => {
+//   const contacts = {
+//     recently: [
+//       {
+//         name: "Nahuel Cristofoli",
+//         //skill:'Ingeniero Industrial',
+//         photo: images.user_andres,
+//         state: true,
+//       },
+//       {
+//         name: "Mariano Busti",
+//         // skill:'Couch Emprendedor',
+//         photo: images.user_mariano,
+//         state: false,
+//       },
+//       {
+//         name: "Marcelo Ponti",
+//         //skill:'Diseñador Grafico',
+//         photo: images.user_marce,
+//         state: true,
+//       },
+//     ],
+//     all: [
+//       {
+//         name: "Alejandro DiLuca",
+//         // skill:'Productor de cine',
+//         photo: images.user_alejandro,
+//         state: true,
+//       },
+//       {
+//         name: "Nicolas Perazzo",
+//         // skill:'Economista',
+//         photo: images.user_nico,
+//         state: false,
+//       },
+//       {
+//         name: "Damian Grimberg",
+//         // skill:'Desarrollador',
+//         photo: images.user_dami,
+//         state: false,
+//       },
+//     ],
+//   };
+
+//   const {
+//     route: { params },
+//   } = props;
+
+//   const { image, title, subtitle } = params;
+
+//   const insets = useSafeAreaInsets();
+
+//   const styles = StyleSheet.create({
+//     container: {
+//       width: "95%",
+//       justifyContent: "flex-start",
+//       alignItems: "center",
+//       backgroundColor: colors.backgroundGrey.primary,
+//     },
+//     catImag: {
+//       width: "100%",
+//       height: 250,
+//       borderRadius: 10,
+//     },
+//     catTitle: {
+//       textAlignVertical: "top",
+//       position: "absolute",
+//       color: "rgba(255,255,255,0.95)",
+//     },
+//     degradado: {
+//       position: "absolute",
+//       width: "100%",
+//       height: "100%",
+//     },
+//     space: {
+//       height: 50,
+//       width: "100%",
+//     },
+//     spaceInitial: {
+//       marginTop: "10%",
+//       marginLeft: "5%",
+//     },
+//     mainContainer: {
+//       marginTop: ui.padding,
+//     },
+//     title: {
+//       paddingLeft: ui.padding + ui.borderRadius.borderRadius / 2,
+//       textAlign: "left",
+//       marginBottom: ui.padding,
+//     },
+//   });
+
+//   return (
+//     <View style={styles.container}>
+//       <ScrollView scrollEventThrottle={1}>
+//         <View style={styles.spaceInitial}>
+//           <Image source={image} style={styles.catImag}></Image>
+//           <LinearGradient style={styles.degradado} {...colors.greyGradient} />
+//           <Text extraStyles={styles.catTitle} {...typography["title-28"]}>
+//             {" "}
+//             {`${title}`}{" "}
+//           </Text>
+//         </View>
+//         <View style={styles.mainContainer}>
+//           {contacts.recently.map((item, index) => (
+//             <ContactsCard contact={item} key={`${item.name}->${index}`} />
+//           ))}
+//           {contacts.all.map((item, index) => (
+//             <ContactsCard contact={item} key={`${item.name}->${index}`} />
+//           ))}
+//         </View>
+//         <View style={styles.space} />
+//       </ScrollView>
+//     </View>
+//   );
+// };
+
+// export default CategoryPreviewScreen;
+
+
